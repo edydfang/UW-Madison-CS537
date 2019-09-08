03e93ad71ea972e4664f4d2d29267ee2357bedab
Yidong Fang <fangyd1997@outlook.com>
2019-09-08 23:03:11 +0800

    format with google style

--- 01_UNIX_Utilities/across.c
+++ 01_UNIX_Utilities/across.c
@@ -1,126 +1,106 @@
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int check_digits(char *input_str)
-{
-    /*
-    check whether all the chars in an char array are digits
-    if ture return 0
-    if false, return 1
-    */
-    int i = 0;
-    while (input_str[i])
-    {
-        if (!isdigit(input_str[i]))
-        {
-            return 1;
-        }
-        i += 1;
+int check_digits(char *input_str) {
+  /*
+  check whether all the chars in an char array are digits
+  if ture return 0
+  if false, return 1
+  */
+  int i = 0;
+  while (input_str[i]) {
+    if (!isdigit(input_str[i])) {
+      return 1;
     }
-    return 0;
+    i += 1;
+  }
+  return 0;
 }
 
-int check_letters(char *input_str)
-{
-    /*
-    check whether all the chars in an char array are letters
-    if ture return 0
-    if false, return 1
-    */
-    int i = 0;
-    while (input_str[i])
-    {
-        if ((input_str[i] < 'a' || input_str[i] > 'z') && input_str[i] != '\n')
-        {
-            return 1;
-        }
-        i += 1;
+int check_letters(char *input_str) {
+  /*
+  check whether all the chars in an char array are letters
+  if ture return 0
+  if false, return 1
+  */
+  int i = 0;
+  while (input_str[i]) {
+    if ((input_str[i] < 'a' || input_str[i] > 'z') && input_str[i] != '\n') {
+      return 1;
     }
-    return 0;
+    i += 1;
+  }
+  return 0;
 }
 
-int main(int argc, char **argv)
-{
-    // check arguments count
-    if (argc < 3 || argc > 4)
-    {
-        fprintf(stderr, "across: invalid number of arguments\n");
-        return 1;
-    }
-    // assign the argument to the vars
-    char *str, *filename;
-    int start_idx, target_len;
-    str = argv[1];
-    if (check_digits(argv[2]) || check_digits(argv[3]))
-    {
-        fprintf(stderr, "across: invalid arguments for start index or word length\n");
-        return 1;
-    }
-    start_idx = strtol(argv[2], NULL, 10);
-    target_len = strtol(argv[3], NULL, 10);
+int main(int argc, char **argv) {
+  // check arguments count
+  if (argc < 3 || argc > 4) {
+    fprintf(stderr, "across: invalid number of arguments\n");
+    return 1;
+  }
+  // assign the argument to the vars
+  char *str, *filename;
+  int start_idx, target_len;
+  str = argv[1];
+  if (check_digits(argv[2]) || check_digits(argv[3])) {
+    fprintf(stderr,
+            "across: invalid arguments for start index or word length\n");
+    return 1;
+  }
+  start_idx = strtol(argv[2], NULL, 10);
+  target_len = strtol(argv[3], NULL, 10);
 
-    if (argc == 5)
-    {
-        filename = argv[4];
-    }
-    else
-    {
-        filename = "/usr/share/dict/words";
-    }
+  if (argc == 5) {
+    filename = argv[4];
+  } else {
+    filename = "/usr/share/dict/words";
+  }
 
-    // open the dictionary file
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL)
-    {
-        fprintf(stderr, "across: cannot open file\n");
-        return 1;
-    }
+  // open the dictionary file
+  FILE *fp = fopen(filename, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "across: cannot open file\n");
+    return 1;
+  }
 
-    if (strlen(str) + start_idx > target_len)
-    {
-        fprintf(stderr, "across: invalid position\n");
-        fclose(fp);
-        return 1;
-    }
-
-    // size 100 is engouth for assmumed condition
-    char line[100];
-    char *line_lower;
-    int result, str_len = strlen(str);
-    int line_len_without_n, line_len;
-    // read file
-    while (fgets(line, 100, fp))
-    {
-        line_len = strlen(line);
-        if (line[strlen(line) - 1] == '\n')
-        {
-            line_len_without_n = line_len - 1;
-        }
-        else
-        {
-            line_len_without_n = line_len;
-        }
-
-        if (line_len_without_n != target_len)
-        {
-            continue;
-        }
-        // compare input string and line beginning at start index
-        result = strncmp(str, line + start_idx, str_len);
-        if (result == 0)
-        {
-            if (check_letters(line))
-            {
-                // if it contains char that is not a letter
-                continue;
-            }
-
-            printf("%s", line);
-        }
-    }
+  if (strlen(str) + start_idx > target_len) {
+    fprintf(stderr, "across: invalid position\n");
     fclose(fp);
+    return 1;
+  }
 
-    return 0;
+  // size 100 is engouth for assmumed condition
+  char line[100];
+  char *line_lower;
+  int result, str_len = strlen(str);
+  int line_len_without_n, line_len;
+  // read file
+  while (fgets(line, 100, fp)) {
+    line_len = strlen(line);
+    if (line[strlen(line) - 1] == '\n') {
+      line_len_without_n = line_len - 1;
+    } else {
+      line_len_without_n = line_len;
+    }
+
+    if (line_len_without_n != target_len) {
+      continue;
+    }
+    // compare input string and line beginning at start index
+    result = strncmp(str, line + start_idx, str_len);
+    if (result == 0) {
+      if (check_letters(line)) {
+        // if it contains char that is not a letter
+        continue;
+      }
+
+      printf("%s", line);
+    }
+  }
+  fclose(fp);
+
+  return 0;
 }
--- 01_UNIX_Utilities/my-diff.c
+++ 01_UNIX_Utilities/my-diff.c
@@ -1,100 +1,84 @@
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void remove_newline(char *line)
-{
-    int str_len = strlen(line);
-    if (line[str_len - 1] == '\n')
-    {
-        line[str_len - 1] = 0;
-    }
+void remove_newline(char *line) {
+  int str_len = strlen(line);
+  if (line[str_len - 1] == '\n') {
+    line[str_len - 1] = 0;
+  }
 }
 
-int main(int argc, char **argv)
-{
-    // check arguments count
-    if (argc != 3)
-    {
-        fprintf(stderr, "mydiff: invalid number of arguments\n");
-        return 1;
-    }
-    // assign the argument to the vars
-    char *fileAname, *fileBname;
-    fileAname = argv[1];
-    fileBname = argv[2];
+int main(int argc, char **argv) {
+  // check arguments count
+  if (argc != 3) {
+    fprintf(stderr, "mydiff: invalid number of arguments\n");
+    return 1;
+  }
+  // assign the argument to the vars
+  char *fileAname, *fileBname;
+  fileAname = argv[1];
+  fileBname = argv[2];
 
-    // open the dictionary file
-    FILE *fpA = fopen(fileAname, "r");
-    FILE *fpB = fopen(fileBname, "r");
-    if (fpA == NULL || fpB == NULL)
-    {
-        fprintf(stderr, "mydiff: cannot open file\n");
-        return 1;
-    }
+  // open the dictionary file
+  FILE *fpA = fopen(fileAname, "r");
+  FILE *fpB = fopen(fileBname, "r");
+  if (fpA == NULL || fpB == NULL) {
+    fprintf(stderr, "mydiff: cannot open file\n");
+    return 1;
+  }
 
-    int lineno = 0;
-    char *lineA = NULL, *lineB = NULL;
-    size_t read_len = 0;
-    int read_returnA, read_returnB;
-    int last_diff = -1;
-    while (1)
-    {
-        lineno += 1;
-        // read line from both files
-        read_returnA = getline(&lineA, &read_len, fpA);
-        read_returnB = getline(&lineB, &read_len, fpB);
-        if (read_returnA == -1 || read_returnB == -1)
-        {
-            break;
-        }
-        // before comparison remove the last newline char
-        remove_newline(lineA);
-        remove_newline(lineB);
-        if (strcmp(lineA, lineB) != 0)
-        {
-            // two line are different
-            if (last_diff + 1 != lineno)
-            {
-                printf("%d\n", lineno);
-            }
-            printf("< %s\n", lineA);
-            printf("> %s\n", lineB);
-            last_diff = lineno;
-        }
+  int lineno = 0;
+  char *lineA = NULL, *lineB = NULL;
+  size_t read_len = 0;
+  int read_returnA, read_returnB;
+  int last_diff = -1;
+  while (1) {
+    lineno += 1;
+    // read line from both files
+    read_returnA = getline(&lineA, &read_len, fpA);
+    read_returnB = getline(&lineB, &read_len, fpB);
+    if (read_returnA == -1 || read_returnB == -1) {
+      break;
     }
-    // TODO: for a longer file, print it
-    if (read_returnA == -1 && read_returnB != -1)
-    {
-        if (last_diff + 1 != lineno)
-        {
-            printf("%d\n", lineno);
-        }
-        do
-        {
-            remove_newline(lineB);
-            printf("> %s\n", lineB);
-        } while (getline(&lineB, &read_len, fpB) != -1);
+    // before comparison remove the last newline char
+    remove_newline(lineA);
+    remove_newline(lineB);
+    if (strcmp(lineA, lineB) != 0) {
+      // two line are different
+      if (last_diff + 1 != lineno) {
+        printf("%d\n", lineno);
+      }
+      printf("< %s\n", lineA);
+      printf("> %s\n", lineB);
+      last_diff = lineno;
     }
-    else if (read_returnA != -1 && read_returnB == -1)
-    {
-        if (last_diff + 1 != lineno)
-        {
-            printf("%d\n", lineno);
-        }
-        do
-        {
-            remove_newline(lineA);
-            printf("< %s\n", lineA);
-        } while (getline(&lineA, &read_len, fpA) != -1);
+  }
+  // for a longer file, print it
+  if (read_returnA == -1 && read_returnB != -1) {
+    if (last_diff + 1 != lineno) {
+      printf("%d\n", lineno);
     }
-    // free the mem for lines
-    free(lineA);
-    free(lineB);
+    do {
+      remove_newline(lineB);
+      printf("> %s\n", lineB);
+    } while (getline(&lineB, &read_len, fpB) != -1);
+  } else if (read_returnA != -1 && read_returnB == -1) {
+    if (last_diff + 1 != lineno) {
+      printf("%d\n", lineno);
+    }
+    do {
+      remove_newline(lineA);
+      printf("< %s\n", lineA);
+    } while (getline(&lineA, &read_len, fpA) != -1);
+  }
+  // free the mem for lines
+  free(lineA);
+  free(lineB);
 
-    // close two files
-    fclose(fpA);
-    fclose(fpB);
-    return 0;
+  // close two files
+  fclose(fpA);
+  fclose(fpB);
+  return 0;
 }
