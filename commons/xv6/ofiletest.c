eada7ae2ff32db25797bdacd3e9a3ebfb6b769e4
Yidong Fang <fangyd1997@outlook.com>
2019-09-21 11:55:54 -0500

    add deleting file; add invalid arg checking

--- commons/xv6/ofiletest.c
+++ commons/xv6/ofiletest.c
@@ -2,7 +2,6 @@
 #include "types.h"
 #include "user.h"
 
-
 int main(int argc, char *argv[]) {
   // check arguments
   if (argc < 2) {
@@ -13,19 +12,26 @@ int main(int argc, char *argv[]) {
   }
   // num of files to open or create
   int num_open = atoi(argv[1]);
+  if (num_open < 1 || num_open > 13) {
+    // too many files to open or invalid input Max16-3=13
+    printf(1, "ofiletest: invalid parameters\n");
+    exit();
+  }
+
   // num of files to close
   int num_close = argc - 2;
   int close_vec[num_close];
   for (int i = 0; i < num_close; i++) {
     close_vec[i] = atoi(argv[i + 2]);
-    if (close_vec[i] < 0 || close_vec[i] > 99) {
+    if (close_vec[i] < 0 || close_vec[i] > num_open-1) {
       printf(1, "ofiletest: invalid parameters\n");
       exit();
     }
   }
   int fd[num_open];
+  char filename[num_open][8];
   for (int i = 0; i < num_open; i++) {
-    char name[8];
+    char* name = filename[i];
     name[0] = 'o';
     name[1] = 'f';
     name[2] = 'i';
@@ -44,6 +50,9 @@ int main(int argc, char *argv[]) {
 
   for (int i = 0; i < num_close; i++) {
     close(fd[close_vec[i]]);
+    // delete this file
+    // int unlink(const char*);
+    unlink(filename[close_vec[i]]);
   }
 
   int ofilecnt = getofilecnt(getpid());
