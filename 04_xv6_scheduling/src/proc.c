6e7d4a0de138cb9cb4e484b88f974052fb5fd241
Yidong Fang <fangyd1997@outlook.com>
2019-10-18 01:29:47 -0500

    add test case 18; problem to be resolved

--- 04_xv6_scheduling/src/proc.c
+++ 04_xv6_scheduling/src/proc.c
@@ -515,42 +515,7 @@ scheduler(void)
         break;
       }
     }
-    if (p!=0){
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
-    }
-    release(&ptable.lock);
-
-  }
-}
-
-void
-scheduler2(void)
-{
-  struct proc *p;
-  struct cpu *c = mycpu();
-  c->proc = 0;
-  
-  for(;;){
-    // Enable interrupts on this processor.
-    sti();
-
-    // Loop over process table looking for process to run.
-    acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
+    if (p!=0){      
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
