bec96b4538892402c1a510b714e79ae55cbab0a9
Yidong Fang <fangyd1997@outlook.com>
2019-12-08 20:57:07 -0600

    small fix; test cases passed

--- 07_xv6_File_System/src_symlink/user.h
+++ 07_xv6_File_System/src_symlink/user.h
@@ -23,7 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-int symlink(char *, char *);
+int symlink(const char *, const char *);
 
 // ulib.c
 int stat(const char*, struct stat*);
